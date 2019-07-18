e627b6ada9eb49275efec58277a0caea9aca742b
KurzGedanke <kurzgedanke@pm.me>
2019-07-18 13:54:39 +0200

    added more projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import projects from './projects.json'
+import './css/projects.css'
 
 export default class Projects extends React.Component{
     constructor(props){
@@ -9,15 +10,21 @@ export default class Projects extends React.Component{
     render() {
         const projects = this.project.map((project) => {
             return (
-            <div>
+            <div className="project">
                 <h2>{project.name}</h2>
-                <h2>{project.plattform}</h2>
+                <span>{project.plattform}</span>
+                <a href={project.url}>Website</a>
+                <a href={project.github}>GitHub</a>
+                <span>{project.description}</span>
+                <span>{project.tools}</span>
+                <span>{project.license}</span>
             </div>
             )
         }
         )
         return(
             <div className="projects">
+                <h1>Coding Projects</h1>
                 {projects}
             </div>
         )
